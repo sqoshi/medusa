8bfb8cc12783a826175e3f3c7a90d46512a7ac99
piotr <piotrpopis98@icloud.com>
2021-06-26 21:47:37 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="medusa",
+	version="1.0.0",
+	description="Photo editor helpful for ML image preprocessing.",
+	url="https://github.com/sqoshi/medusa",
+	author="Piotr Popis",
+	author_email="piotrpopis@icloud.com",
+	license='MIT',
+	packages=[],
+	scripts=[],
+	zip_safe=False)
